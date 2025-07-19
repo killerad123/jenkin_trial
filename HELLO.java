73341854467ec3c1d41102279fd2e4c683d54272
killerad123 <abhidesh1702@gmail.com>
2025-07-19 12:18:01 +0530

    HELLO.java 2nd commit

--- HELLO.java
+++ HELLO.java
@@ -1,6 +1,6 @@
 class HELLO{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("This is a simple Java program.");
+        System.out.prinn("This is a simple Java program.");
     }
 }
